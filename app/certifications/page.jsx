91e52053352a9b8003f4e79a771e62bd10de0d24
Bhargav <bhargavlende11@gmail.com>
2025-12-22 21:08:23 +0530

    commit 1: setup

---
+++ app/certifications/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Certifications = () => {
+  return (
+    <div>certifications</div>
+  )
+}
+
+export default Certifications
---
+++ components/MainNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavLinks from './NavLinks';
+
+const MainNav = () => {
+  return (
+    <nav className='flex flex-col items-center justify-center'>
+        <NavLinks containerStyles="flex flex-col gap-6 w-[100px] mx-auto"
+        />
+    </nav>
+  )
+}
+
+export default MainNav
