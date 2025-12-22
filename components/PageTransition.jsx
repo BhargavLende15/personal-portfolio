24e7c44adfa3542431adb933ef9f9367d383e8e6
Bhargav <bhargavlende11@gmail.com>
2025-12-22 21:36:52 +0530

    commit 2: update00

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,13 +1,13 @@
 "use client";
 
-import {AnimatedPresence} from "framer-motion";
+import {AnimatePresence, motion} from "framer-motion";
 import {usePathname} from "next/navigation";
 
 const PageTransition = ({children}) => {
 
   const pathname = usePathname();
-  return 
-    <AnimatedPresence>
+  return (
+    <AnimatePresence>
         <div key={pathname}>
             <motion.div 
                 initial={{opacity: 1}}
@@ -15,12 +15,12 @@ const PageTransition = ({children}) => {
                     opacity: 0,
                     transition: {delay: 1, duration: 0.4, ease: "easeInOut"},
                 }} 
-                className="min-h-screen w-screen fixed top-0 pointer-events-none"
+                className="min-h-screen w-screen fixed top-0 pointer-events-none bg-primary"
             />
             {children}
             </div>
-    </AnimatedPresence>
-    
+    </AnimatePresence>
+  );
 };
 
 export default PageTransition
