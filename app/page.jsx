68e572c7585d3a85f3ce4850a8205081e8a0ceda
Bhargav <bhargavlende11@gmail.com>
2025-12-24 15:57:04 +0530

    commit 5: update03

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
+import Blob from "@/components/Blob";
 
 const Home = () => {
   return (
@@ -9,8 +10,15 @@ const Home = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center"
     >
-      home
+      <div className="flex flex-col xl:flex-rom items-center justify-between w-full">
+        <div className="w-full xl:w-[550px]">text</div>
+        <div className="hidden xl:block flex-1 relative z-20">
+          {/*blob*/}
+          <Blob/>
+        </div>
+      </div>
     </motion.section>
   );
 };
