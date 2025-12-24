d1a0e038246ff8dd90dc735bdb09209cfbdc995f
Bhargav <bhargavlende11@gmail.com>
2025-12-24 14:40:23 +0530

    commit 4: update02

--- components/Rectangle.jsx
+++ components/Rectangle.jsx
@@ -1,11 +1,10 @@
 "use client"
-import {animate, motion} from "framer-motion";
-import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 // variants
 const rectangleVariants = {
     initial: {
-        y: "-100",
+        y: "-100%",
         height: "100%"
     },
     animate:{
@@ -39,6 +38,16 @@ const Rectangle = () => {
       transition={
       {delay: 0.25, duration: 0.8, ease: [0.63, 0, 0.17, 1],}} 
       className="fixed top-full w-screen h-screen z-20 bg-[#241e36]"  
+    />
+
+    <motion.div 
+      variants={rectangleVariants} 
+      initial="initial" 
+      animate="animate" 
+      exit="exit" 
+      transition={
+      {delay: 0.45, duration: 1, ease: [0.63, 0, 0.17, 1],}} 
+      className="fixed top-full w-screen h-screen z-10 bg-[#2e2743]"  
     />
   </>
 }
